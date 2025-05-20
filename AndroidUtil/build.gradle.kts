7b30a30118b6f8c90d45466c4f63b9d1005dfff3
maicm <chiamingmai@gmail.com>
2025-05-20 16:34:19 +0800

    Add RxJava3Ext

--- AndroidUtil/build.gradle.kts
+++ AndroidUtil/build.gradle.kts
@@ -33,11 +33,13 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.exifinterface)
+    implementation(libs.rxjava)
+    implementation(libs.rxandroid)
+    implementation(libs.rxkotlin)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
